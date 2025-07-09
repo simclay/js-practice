e7533f92447453c70241eaf64281426ad2144229
simclay <shivamb89015@gmail.com>
2025-07-09 18:49:36 +0000

    string and java

---
+++ 02_basics/string.js
@@ -0,0 +1,15 @@
+const name ="shivam"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`); //string interpolation
+
+const gameName = new String('Shivam')
+
+// console.log(gameName[0]);
+
+
+console.log(gameName.length);
+console.log(gameName.length);
+
+
