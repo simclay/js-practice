981459126bfcf2c2c4fb011289713762b1d631f0
simclay <shivamb89015@gmail.com>
2025-07-10 07:55:49 +0000

    practice string and array

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,1 +1,34 @@
-console.log("hello world");
+const myArr =[1,2,3,4]
+// console.log(myArr);
+
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// const new_heros = marvel_heros.concat(dc_heros)
+// console.log(new_heros);
+ 
+const comb_heros = [...marvel_heros,...dc_heros]
+// console.log(comb_heros);
+
+const another_arr = [1,2,3,[4,5,6],7,[6,[5,4]]]
+const real_another_array =another_arr.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Shivam"))
+console.log(Array.from("Shivam"))
+
+let score1=100
+let score2=300
+let score3=400
+console.log(Array.of(score1,score2,score3))
+
+
+
+
+
+
+
+
---
+++ 02_basics/numb.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 2123.9454
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++ Maths+++++++++++++++++++++++
+
+console.log(Math)
+
+// console.log(Math.abs(-100));
+// console.log(Math.round(4.367));
+// console.log(Math.ceil(4.367));
+// console.log(Math.floor(4.367));
+// console.log(Math.min(4,3,5,6));
+
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
